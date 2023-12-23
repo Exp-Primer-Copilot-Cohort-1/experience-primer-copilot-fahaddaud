e620ed4d1edb9b18718a0ddf1632c48a762669a6
fahaddaud <57704392+fahaddaud@users.noreply.github.com>
2023-12-23 10:53:12 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,17 +1,6 @@
-function skillMember() {
+function skillsMember() {
   return {
-    name: 'member',
-    title: 'Member',
-    description: 'Member skill',
-    cost: 1,
-    maxLevel: 1,
-    requirements: [],
-    effects: [
-      {
-        type: 'add',
-        target: 'member',
-        value: 1,
-      },
-    ],
-  };
+    restrict: 'E',
+    templateUrl: 'templates/skill-member.html'
+  }
 }
