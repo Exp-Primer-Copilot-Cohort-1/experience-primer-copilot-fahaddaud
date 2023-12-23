915bc174ef14be10dc41e1480cf4dba2ce70671c
fahaddaud <57704392+fahaddaud@users.noreply.github.com>
2023-12-23 11:02:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,48 +1,21 @@
-//create web server 
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Comment = require('../models/Comment.js');
+//create web server
+const express = require('express');
+const router = express.Router();
+const {Comments} = require('../models');
+const {validateToken} = require('../middlewares/AuthMiddleware');
 
-// GET /comments
-router.get('/', function(req, res, next) {
-  Comment.find(function (err, comments) {
-    if (err) return next(err);
+router.get('/:postId', validateToken, async (req, res) => {
+    const postId = req.params.postId;
+    const comments = await Comments.findAll({where: {PostId: postId}});
     res.json(comments);
-  });
 });
 
-// POST /comments
-router.post('/', function(req, res, next) {
-  Comment.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+router.post('/', validateToken, async (req, res) => {
+    const comment = req.body;
+    const username = req.user.username;
+    comment.username = username;
+    await Comments.create(comment);
+    res.json(comment);
 });
 
-// GET /comments/:id
-router.get('/:id', function(req, res, next) {
-  Comment.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-// PUT /comments/:id
-router.put('/:id', function(req, res, next) {
-  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-// DELETE /comments/:id
-router.delete('/:id', function(req, res, next) {
-  Comment.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-//export router
 module.exports = router;
